bf0f316c66cc03d0ca9e1403288202eddd8a2dff
brandonm <brandon.merkord@alchemysystems.com>
2020-03-28 08:23:01 -0500

    Setting up amplify aws backend

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "note-file-bm-upload-bucket"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://b2xr6iwzxg.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_NGVKdlXGl",
+      APP_CLIENT_ID: "eal7n1psp1mjvfl9u8k11soo4",
+      IDENTITY_POOL_ID: "us-east-2:901ad3e2-234e-4169-a0aa-2163514e1195"
+    }
+  };
