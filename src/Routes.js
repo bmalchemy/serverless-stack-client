d8917d815da9122bfdf1cb1b77c5b9a3e8676263
brandonm <brandon.merkord@alchemysystems.com>
2020-04-14 08:46:13 -0500

    Done building react app

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,37 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 import NotFound from "./containers/NotFound";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      { /* Finally, catch all unmatched routes */ }
-        <Route component={NotFound} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <UnauthenticatedRoute exact path="/login">
+        <Login />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
+        <Signup />
+      </UnauthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings">
+        <Settings />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
+      {/* Finally, catch all unmatched routes */}
+      <Route component={NotFound} />
     </Switch>
   );
 }
